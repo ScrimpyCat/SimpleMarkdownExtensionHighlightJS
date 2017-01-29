f5905f151b1df239f8c0933b7da9dbf719af9394
Stefan Johnson <sj@scrimpycat.io>
2017-01-29 16:35:49 +1100

    Embed renderers in caller instead of library

--- lib/simple_markdown_extension_highlight_js.ex
+++ lib/simple_markdown_extension_highlight_js.ex
@@ -34,31 +34,35 @@ defmodule SimpleMarkdownExtensionHighlightJS do
       no extra work is needed.
     """
 
-    source = Application.get_env(:simple_markdown_extension_highlight_js, :source)
+    defmacro impl_renderers() do
+        source = Application.get_env(:simple_markdown_extension_highlight_js, :source)
 
-    code = if source != nil do
-        case File.read(source) do
-            { :ok, code } -> code
-            { :error, :enoent } ->
-                HTTPoison.start
-                HTTPoison.get!(source).body
+        code = if source != nil do
+            case File.read(source) do
+                { :ok, code } -> code
+                { :error, :enoent } ->
+                    HTTPoison.start
+                    HTTPoison.get!(source).body
+            end
+        else
+            ""
         end
-    else
-        ""
-    end
 
-    includes = Application.get_env(:simple_markdown_extension_highlight_js, :include, [])
-    excludes = Application.get_env(:simple_markdown_extension_highlight_js, :exclude, [])
+        includes = Application.get_env(:simple_markdown_extension_highlight_js, :include, [])
+        excludes = Application.get_env(:simple_markdown_extension_highlight_js, :exclude, [])
 
-    languages = Regex.scan(~r/((?<=registerLanguage\(")|(?<=registerLanguage\(\\"))[^\\"]*/, code)
-    |> Enum.map(fn [lang|_] -> lang end)
-    |> Enum.concat(includes)
-    |> Enum.uniq
-    |> Enum.filter(&(!Enum.any?(excludes, fn lang -> &1 == lang end)))
+        languages = Regex.scan(~r/((?<=registerLanguage\(")|(?<=registerLanguage\(\\"))[^\\"]*/, code)
+        |> Enum.map(fn [lang|_] -> lang end)
+        |> Enum.concat(includes)
+        |> Enum.uniq
+        |> Enum.filter(&(!Enum.any?(excludes, fn lang -> &1 == lang end)))
 
-    for lang <- languages do
-        defimpl SimpleMarkdown.Renderer.HTML, for: String.to_atom("Elixir.SimpleMarkdown.Attribute.PreformattedCode.#{String.capitalize(lang)}") do
-            def render(%{ input: input }), do: SimpleMarkdownExtensionHighlightJS.render(input, unquote(lang))
+        for lang <- languages do
+            quote do
+                defimpl SimpleMarkdown.Renderer.HTML, for: unquote(String.to_atom("Elixir.SimpleMarkdown.Attribute.PreformattedCode.#{String.capitalize(lang)}")) do
+                    def render(%{ input: input }), do: SimpleMarkdownExtensionHighlightJS.render(input, unquote(lang))
+                end
+            end
         end
     end
 
